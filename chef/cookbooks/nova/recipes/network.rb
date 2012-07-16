620307c68d6755e871e0d6c78efd1e878091f711
Christoph Thiel <ct@kki.org>
2012-07-16 12:55:36 +0200

    disable rp_filter, which is enabled by default on SUSE

--- chef/cookbooks/nova/recipes/network.rb
+++ chef/cookbooks/nova/recipes/network.rb
@@ -33,6 +33,7 @@ end
 bash "turn on routing" do
   code <<-'EOH'
 sysctl -w net.ipv4.conf.all.forwarding=1
+sysctl -w net.ipv4.conf.all.rp_filter=0
 EOH
 end
 
@@ -44,10 +45,17 @@ if node.platform != "suse"
     action :create
   end
 else
-  ruby_block "edit sysconfig syslog" do
+  ruby_block "edit sysconfig sysctl" do
     block do
       rc = Chef::Util::FileEdit.new("/etc/sysconfig/sysctl")
       rc.search_file_replace_line(/^IP_FORWARD=/, 'IP_FORWARD="yes"')
+      rc.write_file
+    end
+  end
+  ruby_block "edit sysctl.conf" do
+    block do
+      rc = Chef::Util::FileEdit.new("/etc/sysctl.conf")
+      rc.search_file_replace_line(/^net.ipv4.conf.all.rp_filter/, 'net.ipv4.conf.all.rp_filter = 0')
       rc.write_file
     end
   end
