e99d79883520cdbbae31f7858f7de4ddf70715ae
Victor Lowther <victor.lowther@gmail.com>
2013-05-15 13:14:47 -0500

    Edit how we allocate IP addresses in Nova to cleanly seperate Quantum vs. Nova.

--- crowbar_framework/app/models/nova_service.rb
+++ crowbar_framework/app/models/nova_service.rb
@@ -192,30 +192,35 @@ class NovaService < ServiceObject
     # if tenants are enabled, we don't manage interfaces on nova-fixed.
     #
     net_svc = NetworkService.new @logger
-
     tnodes = role.override_attributes["nova"]["elements"]["nova-multi-controller"]
-    tnodes = all_nodes if role.default_attributes["nova"]["network"]["ha_enabled"]
-    unless tnodes.nil? or tnodes.empty?
+    if role.default_attributes["nova"]["networking_backend"]=="quantum"
       tnodes.each do |n|
-        if role.default_attributes["nova"]["networking_backend"]=="nova-network"
+        net_svc.allocate_ip "default","public","host",n
+      end unless tnodes.nil?
+      quantum = ProposalObject.find_proposal("quantum",role.default_attributes["nova"]["quantum_instance"])
+      all_nodes.each do |n|
+        if quantum["attributes"]["quantum"]["networking_mode"] == "gre"
+          net_svc.allocate_ip "default", "os_sdn", "host", n
+        else
+          net_svc.enable_interface "default", "nova_fixed", n
+        end
+      end unless all_nodes.nil?
+    else
+      tnodes = all_nodes if role.default_attributes["nova"]["network"]["ha_enabled"]
+      unless tnodes.nil? or tnodes.empty?
+        tnodes.each do |n|
           net_svc.allocate_ip "default", "public", "host", n
-          unless role.default_attributes["nova"]["network"]["tenant_vlans"] # or role.default_attributes["nova"]["networking_backend"]=="quantum"
+          unless role.default_attributes["nova"]["network"]["tenant_vlans"]
             net_svc.allocate_ip "default", "nova_fixed", "router", n
           end
         end
-        if role.default_attributes["nova"]["networking_backend"]=="quantum"
+      end
+      unless role.default_attributes["nova"]["network"]["tenant_vlans"]
+        all_nodes.each do |n|
           net_svc.enable_interface "default", "nova_fixed", n
-          net_svc.allocate_ip "default", "public", "host", n
         end
       end
     end
-
-    unless role.default_attributes["nova"]["network"]["tenant_vlans"] or role.default_attributes["nova"]["networking_backend"]=="quantum"
-      all_nodes.each do |n|
-        net_svc.enable_interface "default", "nova_fixed", n
-      end
-    end
-
     @logger.debug("Nova apply_role_pre_chef_call: leaving")
   end
 
