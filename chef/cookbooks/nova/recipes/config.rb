6084854d831bfef1e35b8a0589a7acf48063659c
Tomasz Paszkowski <tpaszkowski@suse.com>
2013-06-19 18:33:51 +0200

    Enable quantum linuxbridge support for nova
    When running nova with quantum on linuxbridge plugin nova compute should
    be aware of it to create appropriate interfaces for vms. This patch
    sets libvirt_vif_driver and linuxnet_interface_driver in nova.conf.

--- chef/cookbooks/nova/recipes/config.rb
+++ chef/cookbooks/nova/recipes/config.rb
@@ -262,6 +262,7 @@ if quantum_servers.length > 0
   else
     per_tenant_vlan=false
   end
+  quantum_networking_plugin = quantum_server[:quantum][:networking_plugin]
   quantum_networking_mode = quantum_server[:quantum][:networking_mode]
 else
   quantum_server_ip = nil
@@ -297,6 +298,7 @@ template "/etc/nova/nova.conf" do
             :quantum_server_port => quantum_server_port,
             :quantum_service_user => quantum_service_user,
             :quantum_service_password => quantum_service_password,
+            :quantum_networking_plugin => quantum_networking_plugin,
             :keystone_service_tenant => keystone_service_tenant,
             :keystone_protocol => keystone_protocol,
             :keystone_address => keystone_address,
