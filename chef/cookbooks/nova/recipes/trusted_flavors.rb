256233fa07bfab237b85bf2dd7ae35478d3dc964
Sergey V. Yudin <tsipa740@gmail.com>
2013-07-04 16:19:28 +0000

    use fqdn istead of admin ip

--- chef/cookbooks/nova/recipes/trusted_flavors.rb
+++ chef/cookbooks/nova/recipes/trusted_flavors.rb
@@ -37,7 +37,7 @@ if node[:nova][:trusted_flavors]
     keystone = node
   end
 
-  keystone_address = Chef::Recipe::Barclamp::Inventory.get_network_by_type(keystone, "admin").address if keystone_address.nil?
+  keystone_address = keystone["fqdn"]
   keystone_service_port = keystone["keystone"]["api"]["service_port"]
   keystone_service_tenant = keystone["keystone"]["service"]["tenant"]
   keystone_service_user = nova["nova"]["service_user"]
@@ -46,18 +46,19 @@ if node[:nova][:trusted_flavors]
   keystone_insecure = keystone_protocol == 'https' && keystone[:keystone][:ssl][:insecure]
   nova_insecure=node[:nova][:ssl][:insecure]
   ssl_insecure = keystone_insecure || nova_insecure
-  param=""
   if ssl_insecure
-    param="--insecure"
+    novacmd = "nova --insecure"
+  else
+    novacmd = "nova"
   end
-  
+
   flavors.keys.each do |id|
     execute "register_#{flavors[id]["name"]}_flavor" do
       command <<-EOF
-        nova #{param} --os_username #{keystone_service_user} --os_password #{keystone_service_password} --os_tenant_name #{keystone_service_tenant} --os_auth_url #{keystone_protocol}://#{keystone_address}:#{keystone_service_port}/v2.0 flavor-create #{flavors[id]["name"]} #{id} #{flavors[id]["mem"]} #{flavors[id]["disk"]} #{flavors[id]["vcpu"]}
-        nova #{param} --os_username #{keystone_service_user} --os_password #{keystone_service_password} --os_tenant_name #{keystone_service_tenant} --os_auth_url #{keystone_protocol}://#{keystone_address}:#{keystone_service_port}/v2.0 flavor-key #{flavors[id]["name"]} set trust:trusted_host=trusted
+        #{novacmd} --os_username #{keystone_service_user} --os_password #{keystone_service_password} --os_tenant_name #{keystone_service_tenant} --os_auth_url #{keystone_protocol}://#{keystone_address}:#{keystone_service_port}/v2.0 flavor-create #{flavors[id]["name"]} #{id} #{flavors[id]["mem"]} #{flavors[id]["disk"]} #{flavors[id]["vcpu"]}
+        #{novacmd} --os_username #{keystone_service_user} --os_password #{keystone_service_password} --os_tenant_name #{keystone_service_tenant} --os_auth_url #{keystone_protocol}://#{keystone_address}:#{keystone_service_port}/v2.0 flavor-key #{flavors[id]["name"]} set trust:trusted_host=trusted
       EOF
-      not_if "nova #{param} --os_username #{keystone_service_user} --os_password #{keystone_service_password} --os_tenant_name #{keystone_service_tenant} --os_auth_url #{keystone_protocol}://#{keystone_address}:#{keystone_service_port}/v2.0 flavor-show #{id}"
+      not_if "#{novacmd} --os_username #{keystone_service_user} --os_password #{keystone_service_password} --os_tenant_name #{keystone_service_tenant} --os_auth_url #{keystone_protocol}://#{keystone_address}:#{keystone_service_port}/v2.0 flavor-show #{id}"
     end
   end
 
