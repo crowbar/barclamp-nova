19ed608612561605437f782ac3b1ddf642944b29
Sergey V. Yudin <tsipa740@gmail.com>
2013-10-11 15:06:18 +0000

    only active git proposal is suitable for PFS deployment

--- crowbar_framework/app/models/nova_service.rb
+++ crowbar_framework/app/models/nova_service.rb
@@ -280,11 +280,7 @@ class NovaService < ServiceObject
 
     if proposal["attributes"][@bc_name]["use_gitrepo"]
       gitService = GitService.new(@logger)
-      gits = gitService.list_active[1]
-      if gits.empty?
-        # No actives, look for proposals
-        gits = gitService.proposals[1]
-      end
+      gits = gitService.list_active[1].to_a
       if not gits.include?proposal["attributes"][@bc_name]["git_instance"]
         raise(I18n.t('model.service.dependency_missing', :name => @bc_name, :dependson => "git"))
       end
