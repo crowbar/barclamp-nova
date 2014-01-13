d83842218436124f05fa375f3a948686669760c2
Vincent Untz <vuntz@suse.com>
2014-01-13 11:57:17 +0100

    Add migration script for nova-network removal

---
+++ chef/data_bags/crowbar/migrate/nova/005_remove_nova-network.rb
@@ -0,0 +1,11 @@
+def upgrade ta, td, a, d
+  a.delete 'networking_backend'
+  a.delete 'network'
+  return a, d
+end
+
+def downgrade ta, td, a, d
+  a[:networking_backend] = ta[:networking_backend]
+  a[:network] = ta[:network]
+  return a, d
+end
