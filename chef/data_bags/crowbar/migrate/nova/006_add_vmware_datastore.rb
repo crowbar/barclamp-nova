9484039d830e9b8d7457fbd769732295ed4ced95
Michal Jura <mjura@suse.com>
2014-02-05 13:59:02 +0100

    Bringing back datastore attribute for Havana

---
+++ chef/data_bags/crowbar/migrate/nova/006_add_vmware_datastore.rb
@@ -0,0 +1,9 @@
+def upgrade ta, td, a, d
+  a[:vcenter][:datastore] = ta[:vcenter][:datastore]
+  return a, d
+end
+
+def downgrade ta, td, a, d
+  a[:vcenter].delete('datastore')
+  return a, d
+end
