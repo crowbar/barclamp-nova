705b692298dc6f11166e10bdd163358e6a77a8a2
Thomas Bechtold <tbechtold@suse.com>
2015-07-10 10:03:49 +0200

    Set cookbook name in metadata
    Needed if the cookbook is used without the crowbar
    framework. I.e. usingthe cookbook as berkshelf dependency.
    Also update maintainer email address.

--- chef/cookbooks/nova/metadata.rb
+++ chef/cookbooks/nova/metadata.rb
@@ -1,9 +1,10 @@
-maintainer       "Opscode, Inc."
-maintainer_email "cookbooks@opscode.com"
-license          "Apache 2.0"
-description      "Installs/Configures nova"
+name "nova"
+maintainer "Crowbar project"
+maintainer_email "crowbar@googlegroups.com"
+license "Apache 2.0"
+description "Installs/Configures nova"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.3"
+version "0.3"
 
 depends "ceph"
 depends "crowbar-openstack"
